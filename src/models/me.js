7e06fd3d69e2b054b0691912257125980aa26d63
lsqzz <2364788985@qq.com>
2019-09-09 11:27:24 +0800

    1

---
+++ src/models/me.js
@@ -0,0 +1,9 @@
+// 这是   个人（getMe setMe）   仓库
+
+export default {
+  namespaced: 'Me',
+
+  state: {},
+  reducer: {},
+  effect: {},
+};
---
+++ src/pages/Me/getMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class GetMe extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>我是getme</h1>
+      </div>
+    );
+  }
+}
+
+export default GetMe;
